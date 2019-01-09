86b92ce50d027c0722bb873837afe81daf73d0cb
jsalamat <salamat_jon@yahoo.com>
2019-01-08 23:18:07 -0500

    add randomdoorgenerator and test work

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,14 +5,43 @@ let doorImage3 = document.getElementById("door3");
 let botDoorPath = "https://jsbotcation.herokuapp.com/assets/images/robot.svg";
 let beachDoorPath = "https://jsbotcation.herokuapp.com/assets/images/beach.svg";
 let spaceDoorPath = "https://jsbotcation.herokuapp.com/assets/images/space.svg";
+let numCloseDoors = 3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
+const randomChoreDoorGenerator = () => {
+	const choreDoor = Math.floor(Math.random() * numCloseDoors);
+
+	if (choreDoor === 0) {
+		openDoor1 = botDoorPath;
+		openDoor2 = beachDoorPath;
+		openDoor3 = spaceDoorPath;
+	} else if ( choreDoor === 1) {
+		openDoor1 = beachDoorPath;
+		openDoor2 = spaceDoorPath;
+		openDoor3 = botDoorPath;
+	}	else if (choreDoor === 2) {
+		openDoor1 = spaceDoorPath;
+		openDoor2 = botDoorPath;
+		openDoor3 = beachDoorPath;
+	}
+};
 
 
 door1.onclick = () => {
-  doorImage1.src = botDoorPath;
+  doorImage1.src = openDoor1;
 };
 door2.onclick = () => {
-  doorImage2.src = beachDoorPath;
+  doorImage2.src = openDoor2;
 };
 door3.onclick = () => {
-  doorImage3.src = spaceDoorPath;
+  doorImage3.src = openDoor3;
 };
+
+
+//Testing randomChoreDorrGenerator();
+// by either refresh or onclick Good luck
+randomChoreDoorGenerator();
+let start = document.getElementById("start")
+start.onclick = randomChoreDoorGenerator;
